5e6265fb72b26573dcac3b1bea5a3084f3c4a591
abhimakulshrestha <abhimakulshrestha2004@gmail.com>
2023-03-27 12:21:33 +0530

    UPDATE

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,56 @@
+import java.sql.SQLOutput;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String[] rps = {"r", "p", "s"};
+        String computerMove = rps[new Random().nextInt(rps.length)];
+
+        Scanner scanner = new Scanner(System.in);
+        String playerMove;
+        while (true) {
+            System.out.println("Please enter your move (r,p,s)");
+            playerMove = scanner.nextLine();
+            if (playerMove.equals("r") || playerMove.equals("p") || playerMove.equals("s")) {
+                break;
+            }
+            System.out.println(playerMove + "is not a valid move.");
+        }
+        System.out.println("Computer played: " + computerMove);
+        if (playerMove.equals(computerMove)) {
+            System.out.println("The game was a tie!");
+
+
+        } else if (playerMove.equals("r")) {
+            if (computerMove.equals("p")) {
+                System.out.println("You loose!");
+            } else if (computerMove.equals("s")) {
+                System.out.println("You win");
+            }
+
+
+        } else if (playerMove.equals("p")) {
+            if (computerMove.equals("r")) {
+                System.out.println("You win!");
+            } else if (computerMove.equals("s")) {
+                System.out.println("You loose!");
+
+
+            }
+
+        }
+
+        else if (playerMove.equals("s")) {
+            if (computerMove.equals("p")) {
+                System.out.println("You win!");
+            } else if (computerMove.equals("r")) {
+                System.out.println("You loose!");
+            }
+
+        }
+
     }
 }
